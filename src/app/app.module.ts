e89390dd8493ca83f6c9fe99833a3ad34c3768e8
Bruno Antunes <bmvantunes@gmail.com>
2017-10-04 12:55:56 +0100

    manual configuration to connect nglg with @angular/cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NglgDemoAppModule } from './nglg';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NglgDemoAppModule
   ],
   providers: [],
   bootstrap: [AppComponent]
